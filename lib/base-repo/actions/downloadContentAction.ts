c78f391b06627fd360a11d9057f84f83b407967d
Thomas Jejkal <thomas.jejkal@kit.edu>
2024-11-28 12:39:46 +0100

    Fixed remaining basePath support

--- lib/base-repo/actions/downloadContentAction.ts
+++ lib/base-repo/actions/downloadContentAction.ts
@@ -2,6 +2,8 @@ import {UrlAction} from "@/lib/base-repo/actions/urlAction";
 
 export class DownloadContentAction extends UrlAction{
     constructor(resourceId:string, filename:string) {
-        super(`/api/download?resourceId=${resourceId}&filename=${filename}&type=data`, "Download", "material-symbols-light:download", 'Download File');
+        const basePath: string = (process.env.NEXT_PUBLIC_BASE_PATH ? process.env.NEXT_PUBLIC_BASE_PATH : "");
+
+        super(`${basePath}/api/download?resourceId=${resourceId}&filename=${filename}&type=data`, "Download", "material-symbols-light:download", 'Download File');
     }
 }
--- lib/base-repo/actions/downloadResourceAction.ts
+++ lib/base-repo/actions/downloadResourceAction.ts
@@ -2,6 +2,8 @@ import {UrlAction} from "@/lib/base-repo/actions/urlAction";
 
 export class DownloadResourceAction extends UrlAction{
     constructor(resourceId:string) {
-        super(`/api/download?resourceId=${resourceId}&type=zip`, "Download", "material-symbols-light:download", 'Download Resource');
+        const basePath: string = (process.env.NEXT_PUBLIC_BASE_PATH ? process.env.NEXT_PUBLIC_BASE_PATH : "");
+
+        super(`${basePath}/api/download?resourceId=${resourceId}&type=zip`, "Download", "material-symbols-light:download", 'Download Resource');
     }
 }
