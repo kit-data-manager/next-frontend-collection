b831d9fd5571aa3e3e8e59e286a461b421740c34
Thomas Jejkal <thomas.jejkal@kit.edu>
2024-12-08 22:53:34 +0100

    Added access permissions tab, finished metadat editing help

--- app/base-repo/components/DataResourceEditor/DataResourceEditor.tsx
+++ app/base-repo/components/DataResourceEditor/DataResourceEditor.tsx
@@ -18,7 +18,6 @@ import {
     DoUpdateDataResource
 } from "@/app/base-repo/components/DataResourceEditor/useDataResourceEditor";
 import ContentInformationCard from "@/app/base-repo/components/ContentInformationCard/ContentInformationCard";
-import {Accordion, AccordionContent, AccordionItem, AccordionTrigger} from "@/components/ui/accordion";
 import {
     Dialog,
     DialogContent, DialogDescription, DialogFooter,
@@ -41,7 +40,7 @@ import {resourcePermissionForUser} from "@/lib/permission-utils";
 import {ToggleTagAction} from "@/lib/base-repo/actions/toggleTagAction";
 import {Tabs, TabsContent, TabsList, TabsTrigger} from "@/components/ui/tabs";
 import {Alert, AlertDescription, AlertTitle} from "@/components/ui/alert";
-import {Terminal, Upload, Info, CircleSlash2} from "lucide-react";
+import {Upload, CircleSlash2, ShieldCheck} from "lucide-react";
 import {Badge} from "@/components/ui/badge";
 import {Icon} from "@iconify/react";
 
@@ -141,16 +140,18 @@ export default function DataResourceEditor({...props}) {
         setOpenModal(false);
     }
 
-
     return (
         <div className="flex flex-grow col-2">
             <div className="flex-grow ">
-                <Tabs defaultValue="account" className="w-full">
+                <Tabs defaultValue={target} className="w-full">
                     <TabsList>
                         <TabsTrigger value="upload"><Upload className="h-4 w-4 mr-2"/> Upload Content</TabsTrigger>
-                        <TabsTrigger value="content"><Icon fontSize={16} icon={"mdi:file-edit-outline"} className="h-4 w-4 mr-2"/> Edit Content</TabsTrigger>
-                        <TabsTrigger value="metadata"><Icon fontSize={16} icon={"material-symbols-light:edit-square-outline"} className="h-4 w-4 mr-2"/>Edit Metadata</TabsTrigger>
-
+                        <TabsTrigger value="content"><Icon fontSize={16} icon={"mdi:file-edit-outline"}
+                                                           className="h-4 w-4 mr-2"/> Edit Content</TabsTrigger>
+                        <TabsTrigger value="metadata"><Icon fontSize={16}
+                                                            icon={"material-symbols-light:edit-square-outline"}
+                                                            className="h-4 w-4 mr-2"/>Edit Metadata</TabsTrigger>
+                        <TabsTrigger value="access"><ShieldCheck className="h-4 w-4 mr-2"/>Access Permissions</TabsTrigger>
                     </TabsList>
                     <TabsContent value="upload">
                         <Alert>
@@ -161,7 +162,8 @@ export default function DataResourceEditor({...props}) {
                                     permissions and
                                     as long as the resource is in state VOLATILE. For uploading, just drag&drop files
                                     from your local
-                                    filesystem to the upload area below or click <Badge className={"text-info"} variant="outline">browse files</Badge> and select one
+                                    filesystem to the upload area below or click <Badge className={"text-info"}
+                                                                                        variant="outline">browse files</Badge> and select one
                                     or more files for upload.
                                     To start the upload. press the upload button.</span>
                                 <br/><br/>
@@ -183,9 +185,10 @@ export default function DataResourceEditor({...props}) {
                                     button, you can  <Badge variant="outline"><Icon
                                         fontSize={16} icon={"material-symbols-light:download"}/> Download</Badge> single files,
                                     or you can <Badge variant="outline"><Icon fontSize={16}
-                                                                               icon={"material-symbols-light:skull-outline"}/> Delete</Badge> files if you have WRITE permissions.
+                                                                              icon={"material-symbols-light:skull-outline"}/> Delete</Badge> files if you have WRITE permissions.
                                     Furthermore, you can mark a file as <Badge variant="thumb_unset">Thumb</Badge> image. A thumb is shown in the resources listing and can be assigned to all files of type jpg, gif, and png
-                                    which are smaller than 100 Kb. Active thumb images are marked with <Badge variant="thumb_set">Thumb</Badge>. If multiple files are marked as thumb, only the latest assignment will count.
+                                    which are smaller than 100 Kb. Active thumb images are marked with <Badge
+                                        variant="thumb_set">Thumb</Badge>. If multiple files are marked as thumb, only the latest assignment will count.
                                 </span>
                                         <br/><br/>
                                         <span className={"text-warn "}>Be aware that removed content cannot be restored. Once a file was removed it has to be re-uploaded.</span>
@@ -217,24 +220,31 @@ export default function DataResourceEditor({...props}) {
                                 <CircleSlash2 className="h-4 w-4"/>
                                 <AlertTitle>No Content Available</AlertTitle>
                                 <AlertDescription>
-                                <span>There was no content uploaded, yet. If you have WRITE permissions, you may switch to the <Badge variant="outline">
+                                <span>There was no content uploaded, yet. If you have WRITE permissions, you may switch to the <Badge
+                                    variant="outline">
                                     <Upload className="h-4 w-4 mr-2"/> Upload Content</Badge> tab and initially upload files.
                                     Otherwise, you may wait until the owner of the resource has uploaded contents.
                                 </span>
                                 </AlertDescription>
                             </Alert>
                         }
-
                     </TabsContent>
                     <TabsContent value="metadata">
                         <Alert>
-                            <Icon fontSize={16} icon={"material-symbols-light:edit-square-outline"} className="h-4 w-4 mr-2"/>
+                            <Icon fontSize={16} icon={"material-symbols-light:edit-square-outline"}
+                                  className="h-4 w-4 mr-2"/>
                             <AlertTitle>Edit Metadata</AlertTitle>
                             <AlertDescription>
-                                <span>Here you can edit the resource&apos;s metadata. Below, you&apos;ll find a form with a pre-selection of available metadata. 
+                                <span>Here you can edit the resource&apos;s metadata. Below, you&apos;ll find a form with a pre-selection of available metadata.
+                                    Mandatory elements are marked with a <span className={"text-error"}>*</span> and must be filled in order to allow to
+                                    <Badge variant="thumb_set" className={"text-ring"}>Commit</Badge> changes or initially store a resource.  Information for
+                                    each element can be shown by hovering over the tooltip icon <a
+                                        className="tooltips">ⓘ</a>. Complex properties are collapsed
+                                    and can be expanded by clicking the expand button <Badge variant="outline"><i
+                                        className={"fas fa-caret-right"}></i></Badge>.
+                                    There are more, optional metadata fields available, which can be made available using the
+                                    <Badge variant="properties"><i className={"fas fa-list mr-2"}></i>properties</Badge> button.
                                 </span>
-                                <br/><br/>
-                                <span className={"text-warn "}>Be aware that removed content cannot be restored. Once a file was removed it has to be re-uploaded.</span>
                             </AlertDescription>
                         </Alert>
                         {editorReady ? null :
@@ -258,8 +268,42 @@ export default function DataResourceEditor({...props}) {
                             />
                         }
                     </TabsContent>
+
+                    <TabsContent value="access">
+                        <Alert>
+                            <ShieldCheck className="h-4 w-4"/>
+                            <AlertTitle>Access Permissions</AlertTitle>
+                            <AlertDescription>
+                                <span>Here you can control who has access to this resource. </span>
+                            </AlertDescription>
+                        </Alert>
+                            <span>Nothing here, yet...</span>
+
+                    </TabsContent>
                 </Tabs>
+
             </div>
+
+            <Dialog open={openModal} modal={true} onOpenChange={closeModal}>
+                <DialogContent className="bg-secondary" onPointerDownOutside={() => {
+                }} onInteractOutside={() => {
+                }}>
+                    <DialogHeader>
+                        <DialogTitle>Add New Tag</DialogTitle>
+                        <DialogDescription className="secondary">
+                            Provide a tag to add to this content element.
+                        </DialogDescription>
+                        <div className="grid w-full max-w-sm items-center gap-1.5">
+                            <Input type="text" id="tag" placeholder="newTag" className="bg-secondary border-1"
+                                   onChange={(event: any) => setTag(event.target.value)}/>
+                        </div>
+                    </DialogHeader>
+                    <DialogFooter>
+                        <Button onClick={() => assignTag()}
+                                className={"bg-accent text-accent-foreground"}>Add</Button>
+                    </DialogFooter>
+                </DialogContent>
+            </Dialog>
         </div>
     )
 
@@ -341,26 +385,7 @@ export default function DataResourceEditor({...props}) {
             <img src={thumb}
                  className={"w-48 min-w-6 ml-6"}/>
         </div>
-        <Dialog open={openModal} modal={true} onOpenChange={closeModal}>
-            <DialogContent className="bg-secondary" onPointerDownOutside={() => {
-            }} onInteractOutside={() => {
-            }}>
-                <DialogHeader>
-                    <DialogTitle>Add New Tag</DialogTitle>
-                    <DialogDescription className="secondary">
-                        Provide a tag to add to this content element.
-                    </DialogDescription>
-                    <div className="grid w-full max-w-sm items-center gap-1.5">
-                        <Input type="text" id="tag" placeholder="newTag" className="bg-secondary border-1"
-                               onChange={(event: any) => setTag(event.target.value)}/>
-                    </div>
-                </DialogHeader>
-                <DialogFooter>
-                    <Button onClick={() => assignTag()}
-                            className={"bg-accent text-accent-foreground"}>Add</Button>
-                </DialogFooter>
-            </DialogContent>
-        </Dialog>
+
     </div>
     )*/
 }
