c4adee55753e72060e394f5a24c00f52fc1f6df9
Thomas Jejkal <thomas.jejkal@kit.edu>
2025-12-17 12:46:11 +0100

    Started migration of shadcn components, still problematic

--- components/MainMenu/MainMenu.tsx
+++ components/MainMenu/MainMenu.tsx
@@ -73,29 +73,37 @@ interface ListItemProps extends React.ComponentPropsWithoutRef<typeof Slot> {
     href?: string;
 }
 
-const ListItem = React.forwardRef<HTMLAnchorElement, ListItemProps>(
-    ({ className, title, children, ...props }, ref) => {
-        return (
-            <li>
-                <NavigationMenuLink asChild>
-                    <Link
-                        href={props.href || "#"}
-                        className={cn(
-                            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
-                            className
-                        )}
-                    >
-                        <div>
-                            <div className="text-sm font-medium leading-none">{title}</div>
-                            <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
-                                {children}
-                            </p>
-                        </div>
-                    </Link>
-                </NavigationMenuLink>
-            </li>
-
-        )
+const ListItem = (
+    {
+        ref,
+        className,
+        title,
+        children,
+        ...props
+    }: ListItemProps & {
+        ref?: React.RefObject<HTMLAnchorElement>;
     }
-)
+) => {
+    return (
+        <li>
+            <NavigationMenuLink asChild>
+                <Link
+                    href={props.href || "#"}
+                    className={cn(
+                        "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+                        className
+                    )}
+                >
+                    <div>
+                        <div className="text-sm font-medium leading-none">{title}</div>
+                        <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+                            {children}
+                        </p>
+                    </div>
+                </Link>
+            </NavigationMenuLink>
+        </li>
+
+    )
+}
 ListItem.displayName = "ListItem"
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -5,7 +5,9 @@ import {cva, type VariantProps} from "class-variance-authority"
 import {cn} from "@/lib/general/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background " +
+    "transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring " +
+    "focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
@@ -36,24 +38,30 @@ const buttonVariants = cva(
   }
 )
 
-export interface ButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-    VariantProps<typeof buttonVariants> {
-  asChild?: boolean
+export type ButtonProps =
+React.ComponentPropsWithoutRef<"button"> &
+VariantProps<typeof buttonVariants> & {
+    asChild?: boolean
+    ref?: React.Ref<HTMLButtonElement>
 }
 
-const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
-    const Comp = asChild ? Slot : "button"
-    return (
-      <Comp
-        className={cn(buttonVariants({ variant, size, className }))}
-        ref={ref}
-        {...props}
-      />
-    )
-  }
-)
+export function Button({
+                           className,
+                           variant,
+                           size,
+                           asChild = false,
+                           ref,
+                           ...props
+                       }: ButtonProps) {
+  const Comp = asChild ? Slot : "button"
+  return (
+    <Comp
+      className={cn(buttonVariants({ variant, size, className }))}
+      ref={ref}
+      {...props}
+    />
+  )
+}
 Button.displayName = "Button"
 
-export { Button, buttonVariants }
+export { buttonVariants }
